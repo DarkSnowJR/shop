8a4a04b4d4d8cf0bda1c62eb15cfd353cf586759
DarkSnowJR <amirkiankiani@icloud.com>
2021-07-05 10:46:57 +0430

    update car funcs

--- cart/cart.py
+++ cart/cart.py
@@ -15,19 +15,28 @@ class Cart(object):
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
-        def add(self, product, quantity=1, override_quantity=False):
-            """
-            Add a product to the cart or update its quantity.
-            """
-            product_id = str(product.id)
-            if product_id not in self.cart:
-                self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
-            if override_quantity:
-                self.cart[product_id] ['quantity'] = quantity
-            else:
-                self.cart[product_id] ['quantity'] += quantity
-            self.save()
+    def add(self, product, quantity=1, override_quantity=False):
+        """
+        Add a product to the cart or update its quantity.
+        """
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
+        if override_quantity:
+            self.cart[product_id] ['quantity'] = quantity
+        else:
+            self.cart[product_id] ['quantity'] += quantity
+        self.save()
 
-        def save(self):
-            # mark the session as "modified" to make sure it gets saved
-            self.session.modified = True
+    def save(self):
+        # mark the session as "modified" to make sure it gets saved
+        self.session.modified = True
+
+    def remove(self, product):
+        """
+        Remove a product from the cart.
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
